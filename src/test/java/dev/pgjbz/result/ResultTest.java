2417933dd795edbc6905d436e5b2f0e4887a9766
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2023-10-20 20:39:53 -0300

    test: fix warnings

--- src/test/java/dev/pgjbz/result/ResultTest.java
+++ src/test/java/dev/pgjbz/result/ResultTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 import dev.pgjbz.result.exception.ValueNotCompatibleException;
@@ -26,7 +25,8 @@ public class ResultTest {
     @Test
     void shouldBeErrorAndThrowsError() {
         final var err = Result.err("error");
-        assertThrows(ValueNotCompatibleException.class, () -> err.unwrap());
+        final ValueNotCompatibleException ex = assertThrows(ValueNotCompatibleException.class, () -> err.unwrap());
+        assertEquals("not ok value", ex.getMessage());
     }
 
     @Test
@@ -34,15 +34,16 @@ public class ResultTest {
         final Result<String, String> err = Result.err("1");
         final var mapped = err
             .map(ok -> ok.concat("123"))
-            .mapErr(e -> Integer.parseInt(e)).okOrDefault("def");
+            .mapErr(Integer::parseInt)
+            .okOrDefault("def");
         assertEquals("def", mapped);
     }
 
     @Test
     void shouldMapOk() {
         final Result<String, String> ok = Result.ok("2");
-        final var mapped = ok.mapErr(e -> e.concat("2"))
-                .map(o -> Integer.parseInt(o))
+        final Integer mapped = ok.mapErr(e -> e.concat("2"))
+                .map(Integer::parseInt)
                 .unwrap();
         assertEquals(2, mapped);
     }
@@ -52,7 +53,7 @@ public class ResultTest {
         final Result<String, String> err = Result.err("err");
         switch(err) {
             case Err<String, String>(var e) -> assertEquals("err", e);
-            case Ok<String, String>(var ok) -> assertTrue(false);
+            case Ok<String, String>(var ok) -> assertEquals("ok", ok);
             default -> assertTrue(false);
         }        
     }
@@ -63,7 +64,7 @@ public class ResultTest {
         final Result<Person, String> ok = Result.ok(new Person(10));
         switch (ok) {
             case Ok(var p) when p.age() > 18 -> assertTrue(false);
-            case Ok(var p) -> assertTrue(true);
+            case Ok(var p) -> assertTrue(p.age < 18);
             default -> assertTrue(false);
         }
     }
